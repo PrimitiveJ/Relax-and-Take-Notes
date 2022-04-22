e2c5ed73ce88a0281a22a4525257101012c0b0c9
Primitive J <hokecj@gmail.com>
2022-04-21 20:08:28 -0400

    initial commit with created server.js file and structuring

---
+++ Develop/server.js
@@ -0,0 +1,30 @@
+// List of Dependencies
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+const express = require("express");
+
+
+//Async Processes
+
+//Set up the Server
+
+//Middleware(static)
+
+//API Routes = *API----------------------------------------
+
+//"GET" request *API
+
+//"POST" request *API
+
+//"DELETE" request *API
+
+//END OF API ROUTES----------------------------------------
+
+//HTML Routes
+
+//App Listening
+app.listen(PORT, function(){
+    console.log("App listening on port" + PORT)
+});
+
